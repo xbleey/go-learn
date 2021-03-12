06280fa9cf6cba3831d06366eea10dad56c909a5
胡俊豪 <jimhu@digitalgd.com.cn>
2021-03-12 15:02:15 +0800

    v1.0.1 replace txt to html

--- go.mod
+++ go.mod
@@ -2,4 +2,19 @@ module hello
 
 go 1.15
 
-require github.com/go-sql-driver/mysql v1.5.0
+require (
+	github.com/gin-gonic/gin v1.6.3
+	github.com/go-playground/validator/v10 v10.4.1 // indirect
+	github.com/golang/protobuf v1.4.3 // indirect
+	github.com/json-iterator/go v1.1.10 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/ramya-rao-a/go-outline v0.0.0-20200117021646-2a048b4510eb // indirect
+	github.com/ugorji/go v1.2.2 // indirect
+	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad // indirect
+	golang.org/x/sys v0.0.0-20201231184435-2d18734c6014 // indirect
+	golang.org/x/tools v0.0.0-20210102185154-773b96fafca2 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
--- src/main.go
+++ src/main.go
@@ -2,24 +2,57 @@ package main
 
 import (
 	"fmt"
+	"github.com/gin-gonic/gin"
 	"io/ioutil"
-	"net"
+	"net/http"
 	"os"
 )
 
 func main() {
-	service := "8.129.102.0:80"
-	tcpAddr, err := net.ResolveTCPAddr("tcp4", service)
+	r := gin.Default()
+	result, err := ReadAll("index.html")
 	checkError(err)
-	conn, err := net.DialTCP("tcp", nil, tcpAddr)
-	checkError(err)
-	_, err = conn.Write([]byte("HEAD / HTTP/1.0\r\n\r\n"))
-	checkError(err)
-	result, err := ioutil.ReadAll(conn)
-	checkError(err)
-	fmt.Println(string(result))
-	os.Exit(0)
+	r.GET("/", func(c *gin.Context) {
+		c.Writer.WriteHeader(200)
+		c.Writer.Write(result)
+	})
+	r.GET("/user/:name", func(c *gin.Context) {
+		name := c.Param("name")
+		c.String(http.StatusOK, "Hello %s", name)
+	})
+
+	r.GET("/users", func(c *gin.Context) {
+		name := c.Query("name")
+		role := c.DefaultQuery("role", "teacher")
+		c.String(http.StatusOK, "%s is a %s", name, role)
+	})
+
+	r.POST("/posts", func(c *gin.Context) {
+		id := c.Query("id")
+		page := c.DefaultQuery("page", "0")
+		username := c.PostForm("username")
+		password := c.DefaultPostForm("username", "000000") // 可设置默认值
+
+		c.JSON(http.StatusOK, gin.H{
+			"id":       id,
+			"page":     page,
+			"username": username,
+			"password": password,
+		})
+	})
+
+	/*8888端口*/
+	r.Run(":8888")
 }
+
+func ReadAll(filePth string) ([]byte, error) {
+	f, err := os.Open(filePth)
+	if err != nil {
+		return nil, err
+	}
+	return ioutil.ReadAll(f)
+}
+
 func checkError(err error) {
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
